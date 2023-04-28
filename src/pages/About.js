e57b93aac73419e94e98e61962a9996cfdf7fd99
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-04-28 16:07:58 +0200

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className=" section about-section">
+      <h2 className="section-title">about us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. A voluptatibus
+        at suscipit delectus. Voluptatum adipisci dolores voluptate blanditiis
+        reprehenderit reiciendis aspernatur quod earum pariatur tempore, eum
+        odio culpa, minus rem.
+      </p>
+    </section>
+  );
+};
+
+export default About;
